1a93fb297d109433cd62d8c239bf4fca860de03f
Kheiner Ivan Murillo Mosquera <kheinermurillomosquera@gmail.com>
2021-11-30 11:15:50 -0500

    login form updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+// import React, { Component } from "react";
+// import ReactDOM from 'react-dom';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Container } from 'react-bootstrap';
+import Login from './components/login/login';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-
+    <div className="App">
+      <Container>
+        <Login/>
+      </Container>
     </div>
   );
 }
---
+++ src/components/login/login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+
+export default class login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <Container id="login-container" style={{ marginTop: 200 }}>
+                <Form>
+                    <Form.Group>
+                        <Form.Label style={{ float: 'left' }}>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group>
+                        <Form.Label style={{ float: 'left' }}>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    {/* <Form.Group>
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group> */}
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
+        );
+    }
+}
