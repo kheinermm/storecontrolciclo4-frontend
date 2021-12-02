c3b012c441353cfb1e674cbba37a2aea16ecea17
Kheiner Ivan Murillo Mosquera <kheinermurillomosquera@gmail.com>
2021-12-01 21:40:09 -0500

    ok
    ok

--- src/components/auth/privaterouter.js
+++ src/components/auth/privaterouter.js
@@ -1,43 +1,40 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router';
+//import { Route, Redirect } from 'react-router-dom';
 import { getSession } from '../helper/helper';
 
 const checkAuth = () => {
-    return !getSession() ? false : true;
-   };
+	return !getSession() ? false : true;
+};
 
-export default class privaterouter extends React.Component {
-       constructor(props) {
-        super(props);
-        this.state = {
-            auth: false
-         }
-      }
+export default class PrivateRoute extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			auth: false
+		}
+	}
 
-      //WARNING! To be deprecated in React v17. Use componentDidMount instead.
-      componentWillMount() {
-        this.setState({
-            auth: checkAuth() && !this.state.auth
-            })
-      }
+	componentWillMount() {
+		this.setState({
+			auth: checkAuth() && !this.state.auth
+		})
+	}
 
-      render(){
-        const { component: Component, ...rest } = this.props;
-       
-       return (
-        <Route
-         {...rest}
-         render={(props) => {
-           this.state.auth ? (
-            <Component {...props} />
-        ) : (
-        <Redirect
-          to={{ pathname: '/login', state:
-          { from: this.props.location }}}
-            />
-           );
-          }}
-        />
-      );
-     }
-    }
+	render() {
+
+		const { component: Component, ...rest } = this.props;
+
+		return ( 
+			<Route
+			{...rest}
+			render={(props) => {
+				this.state.auth 
+					? ( <Component {...props} /> )
+					: ( <Redirect to={{ pathname: '/login', 
+						state: { from: this.props.location } }} /> );
+			}}
+			/>
+		);
+	}
+}
